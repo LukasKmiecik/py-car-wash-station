5248848824afe47fe7299145f2d3dfd1933050fe
Lukasz Kmiecik <kmieciklu@gmail.com>
2025-09-09 22:54:06 +0200

    new solution + flake

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ class Car:
         self.clean_mark = clean_mark
         self.brand = brand
 
+
 class CarWashStation:
     def __init__(
             self,
@@ -19,10 +20,12 @@ class CarWashStation:
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
-        self.average_rating = round(average_rating, 1)  # zaokrąglenie na wejściu
+        self.average_rating = round(
+            average_rating, 1
+        )
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list['Car']) -> float:
+    def serve_cars(self, cars: list["Car"]) -> float:
         price = 0.0
         for car in cars:
             difference = self.clean_power - car.clean_mark
@@ -31,14 +34,14 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(price, 1)
 
-    def calculate_washing_price(self, car: 'Car') -> float:
+    def calculate_washing_price(self, car: "Car") -> float:
         difference = self.clean_power - car.clean_mark
         if difference <= 0:
             return 0.0
         raw_cost = car.comfort_class * difference * self.average_rating
         return round(raw_cost / self.distance_from_city_center, 1)
 
-    def wash_single_car(self, car: 'Car') -> None:
+    def wash_single_car(self, car: "Car") -> None:
         difference = self.clean_power - car.clean_mark
         if difference > 0:
             car.clean_mark = self.clean_power
